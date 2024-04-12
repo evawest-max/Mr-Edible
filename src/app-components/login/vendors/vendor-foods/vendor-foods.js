fc4d86d2784dfc6d158b8493cf6032225ad41ca4
evawest-max <Ebuoluwaakinwumi@gmail.com>
2024-04-12 13:33:54 +0100

    changed

--- src/app-components/login/vendors/vendor-foods/vendor-foods.js
+++ src/app-components/login/vendors/vendor-foods/vendor-foods.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import "./vendor-foods.css"
 import VendorFoodCard from '../components/vendor food card/vendorFoodCard'
-import { getDatabase, ref, onValue, remove, set, update } from "firebase/database";
-import { getDownloadURL, ref as storageref, getStorage } from 'firebase/storage';
+import { getDatabase, ref, onValue, update } from "firebase/database";
+// import { getDownloadURL, ref as storageref, getStorage } from 'firebase/storage';
 
 export default function VendorFoods() {
   let food=[]
