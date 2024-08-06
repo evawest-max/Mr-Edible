03e5e843e12ed171e50a1120d6b6aa39faca9118
evawest-max <Ebuoluwaakinwumi@gmail.com>
2024-08-06 14:09:53 +0100

    ..

--- src/app-components/component/vendor-component/About vendor/about-vendor.js
+++ src/app-components/component/vendor-component/About vendor/about-vendor.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import "./about-vendor.css"
 import { getDownloadURL, getStorage, ref as storageref} from 'firebase/storage'
-import { storage } from '../../firebase component/firebase config'
 
-export default function AboutVendor({id, bussinessName, logo, rating, about, storeStatus, address, email, phonenumber}) {
+
+export default function AboutVendor() {
+    let vendor=JSON.parse(localStorage.getItem("vendors"))
     const storage= getStorage()
     let [pic, setPic]=useState("https://robohash.org/vendor")
-    getDownloadURL(storageref(storage, `${logo}`))
+    getDownloadURL(storageref(storage, `${vendor[0].passport}`))
     .then((url) => {
         setPic(url )
         // alert(url)
@@ -15,20 +16,20 @@ export default function AboutVendor({id, bussinessName, logo, rating, about, sto
         console.log(error)
         setPic("https://robohash.org/vendor")
     })
-
+    console.log(vendor)
     return (
     <div>
-        <div id='Vendors-personal-info'>
-                <img src={logo} alt='logo'/>
-                <h3>{bussinessName}</h3>
-                <p>{email}</p>
-                <p>{phonenumber}</p>
-                <p>{address}</p>
-                <p>{rating}</p>
-                <h4>{storeStatus? "CLOSE":"OPEN"}</h4>
+        <div id='Vendors-personal-info-about'>
+                <img src={pic} alt='logo'/>
+                <h2>{vendor[0].bussiness_name}</h2>
+                <p>{vendor[0].email}</p>
+                <p>{vendor[0].phonenumber}</p>
+                <p>{vendor[0].address}</p>
+                <p>We are rated {vendor[0].rating} stars</p>
+                <h4>We are <b>{vendor[0].store_status===false? "CLOSE for now, check back later":"OPEN to serve you 🥰"}</b></h4>
         </div>
         <div>
-            <p id='vendor-about-us'><b><u>About Us:</u> </b>{about}</p>
+            <p id='vendor-about-us'><b><u>About Us:</u> </b>{vendor[0].aboutUs}</p>
         </div>
     </div>
   )
